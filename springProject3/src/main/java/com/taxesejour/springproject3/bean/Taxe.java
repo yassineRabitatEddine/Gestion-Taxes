a360ab62950c5614574870358afc01c463b98720
GhitaBenchekroun <ghita.ben201@gmail.com>
2022-08-11 20:59:08 +0100

    taxe/taux/categorie

--- springProject3/src/main/java/com/taxesejour/springproject3/bean/Taxe.java
+++ springProject3/src/main/java/com/taxesejour/springproject3/bean/Taxe.java
@@ -31,5 +31,67 @@ public class Taxe {
     @OneToOne
     private Taux taux ;
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getAnnee() {
+        return annee;
+    }
+
+    public void setAnnee(int annee) {
+        this.annee = annee;
+    }
+
+    public int getTrimestre() {
+        return trimestre;
+    }
+
+    public void setTrimestre(int trimestre) {
+        this.trimestre = trimestre;
+    }
+
+    public double getCA() {
+        return CA;
+    }
+
+    public void setCA(double CA) {
+        this.CA = CA;
+    }
+
+    public double getMontantBase() {
+        return montantBase;
+    }
+
+    public void setMontantBase(double montantBase) {
+        this.montantBase = montantBase;
+    }
+
+    public Redevable getRedevable() {
+        return redevable;
+    }
+
+    public void setRedevable(Redevable redevable) {
+        this.redevable = redevable;
+    }
+
+    public Local getLocal() {
+        return local;
+    }
+
+    public void setLocal(Local local) {
+        this.local = local;
+    }
+
+    public Taux getTaux() {
+        return taux;
+    }
+
+    public void setTaux(Taux taux) {
+        this.taux = taux;
+    }
 }
